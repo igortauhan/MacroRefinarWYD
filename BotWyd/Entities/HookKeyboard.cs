6a1ddec95cf6a19274f70f613ffcd008b8a0b568
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-05-27 00:40:27 -0300

    HookKeyboard feito e classe Macro feita

---
+++ BotWyd/Entities/HookKeyboard.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotWyd.Entities
+{
+    class HookKeyboard
+    {
+        private static int[] _coordenadas = new int[2];
+        private static int[] _coordenadasSlot = new int[2];
+
+        private static Macro _macro = new Macro();
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == 33)
+                {
+                    try
+                    {
+                        _macro.Refinar(_coordenadas[0], _coordenadas[1], _coordenadasSlot[0], _coordenadasSlot[1]);
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine("Ocorreu um erro: " + error.ToString());
+                    }
+                }
+
+                if (vkCode == 123)
+                {
+                    _coordenadas = _macro.GetCoordenadas();
+                }
+
+                if (vkCode == 121)
+                {
+                    _coordenadasSlot = _macro.GetCoordenadas();
+                }
+
+                if (vkCode == 34)
+                {
+                    UnhookWindowsHookEx(_hookID);
+                    Application.Exit();
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
---
+++ BotWyd/Entities/Macro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace BotWyd.Entities
+{
+    class Macro
+    {
+        private InputSimulator _macro = new InputSimulator();
+
+        public int[] GetCoordenadas()
+        {
+            Point posicaoMouse = new Point();
+
+            int[] coordenadas = new int[2];
+            coordenadas[0] = posicaoMouse.X;
+            coordenadas[1] = posicaoMouse.Y;
+
+            return coordenadas;
+        }
+
+        public void Refinar(int coordenadaX, int coordenadaY, int coordenadaXslot, int coordenadaYslot)
+        {
+            _macro.Mouse.MoveMouseTo(coordenadaXslot, coordenadaYslot);
+            _macro.Mouse.LeftButtonClick();
+
+            _macro.Mouse.MoveMouseTo(coordenadaX, coordenadaY);
+            _macro.Mouse.LeftButtonClick();
+        }
+    }
+}
