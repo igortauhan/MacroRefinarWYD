e145c14ae12d7ebeb0851bc7e191d6a1dd3c2304
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-05-27 10:16:51 -0300

    Adicioanis na interface

--- BotWyd/Form1.Designer.cs
+++ BotWyd/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace BotWyd
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -54,11 +55,21 @@ namespace BotWyd
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Feito por: Igor";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(257, 331);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -67,6 +78,7 @@ namespace BotWyd
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Macro Refinar";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +86,7 @@ namespace BotWyd
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- BotWyd/Form1.cs
+++ BotWyd/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using BotWyd.Entities;
 
@@ -29,7 +22,7 @@ namespace BotWyd
             MessageBox.Show("Para gravar as coordenadas de onde você deixar a PL na mochila, aperte HOME.\n" +
                 "Para gravar as coordenadas do item que você quer refinar, aperte END\n" +
                 "Para rodar o macro, aperte PgUp (primeiro grave as coordenadas)\n" +
-                "Caso você prefira, poderá fechar o programa apertando PgDn");
+                "SAIA DO PROGRAMA APERTANDO PgDn");
         }
     }
 }
